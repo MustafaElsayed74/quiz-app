6d6a90e3b08a9ba74c143e072cac4cd103bb78c8
0x0desha74 <mudesha74@gmail.com>
2025-09-21 01:33:11 +0300

    addQuestions service

--- src/main/java/com/mustafa/quizapp/controller/QuestionController.java
+++ src/main/java/com/mustafa/quizapp/controller/QuestionController.java
@@ -2,9 +2,12 @@ package com.mustafa.quizapp.controller;
 
 import com.mustafa.quizapp.domain.entity.Question;
 import com.mustafa.quizapp.service.QuestionService;
+import jakarta.annotation.Nullable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -22,8 +25,15 @@ public class QuestionController {
         return ResponseEntity.ok(questionService.getAllQuestions());
     }
 
-    @GetMapping("/{category}")
+    @GetMapping("/category/{category}")
     public ResponseEntity<List<Question>> getByCategory(@PathVariable String category){
         return ResponseEntity.ok(questionService.getAllByCategory(category));
+    }
+
+    @PostMapping
+    public ResponseEntity<Question> addQuestion(@RequestBody Question question){
+        Question createdQuestion = questionService.addQuestion(question);
+
+        return new ResponseEntity<>(createdQuestion, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/mustafa/quizapp/service/QuestionService.java
+++ src/main/java/com/mustafa/quizapp/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.mustafa.quizapp.domain.entity.Question;
 import com.mustafa.quizapp.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -21,5 +22,9 @@ public class QuestionService {
 
     public List<Question> getAllByCategory(String category){
         return questionRepository.findByCategory(category);
+    }
+
+    public Question addQuestion(Question question){
+        return  questionRepository.save(question);
     }
 }
